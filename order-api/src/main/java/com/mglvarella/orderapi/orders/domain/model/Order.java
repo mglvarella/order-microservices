b77fefc0a344597a415e9ec2b0e771976130fbc1
Miguel Varella <mig-varella1@hotmail.com>
2026-03-02 20:28:38 -0300

    Finishing domain and setting entity

--- order-api/src/main/java/com/mglvarella/orderapi/orders/domain/model/Order.java
+++ order-api/src/main/java/com/mglvarella/orderapi/orders/domain/model/Order.java
@@ -1,15 +1,85 @@
 package com.mglvarella.orderapi.orders.domain.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String customerName;
     private String customerEmail;
     private LocalDateTime orderDate;
     private OrderStatus status;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> items;
     private BigDecimal totalAmount;
+
+    public Order() {
+    }
+
+    public Order(
+            String customerName,
+            String customerEmail,
+            List<OrderItem> items) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.items = items;
+        if (items != null) {
+            items.forEach(item -> item.setOrder(this));
+        }
+        this.orderDate = LocalDateTime.now();
+        this.status = OrderStatus.PENDING;
+        this.calculateTotalAmount();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerEmail() {
+        return this.customerEmail;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return this.orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return this.status;
+    }
+
+    public BigDecimal calculateTotalAmount() {
+        this.totalAmount = (items == null) ? BigDecimal.ZERO
+                : items.stream()
+                        .map(OrderItem::getSubTotal)
+                        .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return this.totalAmount;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void addItem(OrderItem item) {
+        this.items.add(item);
+        item.setOrder(this);
+        calculateTotalAmount();
+    }
 }
--- order-api/src/main/java/com/mglvarella/orderapi/orders/domain/model/OrderItem.java
+++ order-api/src/main/java/com/mglvarella/orderapi/orders/domain/model/OrderItem.java
@@ -1,11 +1,74 @@
 package com.mglvarella.orderapi.orders.domain.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "order_items")
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
     private String productName;
     private Integer quantity;
     private BigDecimal unitPrice;
     private BigDecimal subTotal;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(
+            String productName,
+            Integer quantity,
+            BigDecimal unitPrice) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.calculateSubTotal();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public BigDecimal getSubTotal() {
+        return subTotal;
+    }
+
+    public void calculateSubTotal() {
+        this.subTotal = this.unitPrice.multiply(BigDecimal.valueOf(this.quantity));
+    }
 }
