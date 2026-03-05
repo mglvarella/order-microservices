8e7b86fe625de26421deb0dd48d57a377b89887c
Miguel Varella <mig-varella1@hotmail.com>
2026-03-05 19:28:59 -0300

    Creating user model

---
+++ gateway-api/src/main/java/com/mglvarella/gatewayapi/auth/domain/model/User.java
@@ -0,0 +1,46 @@
+package com.mglvarella.gatewayapi.auth.domain.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private List<String> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public String getPassword() { return this.password; }
+
+    @Override
+    public String getUsername() { return this.username; }
+
+    public List<String> getRoles() {
+        return this.roles;
+    }
+}
