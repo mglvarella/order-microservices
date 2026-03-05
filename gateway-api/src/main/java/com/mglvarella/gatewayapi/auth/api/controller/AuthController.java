0a54c86d514e234a844adb91d9c3628e772d56d1
Miguel Varella <mig-varella1@hotmail.com>
2026-03-05 20:04:17 -0300

    Fixed login path

--- gateway-api/src/main/java/com/mglvarella/gatewayapi/auth/api/controller/AuthController.java
+++ gateway-api/src/main/java/com/mglvarella/gatewayapi/auth/api/controller/AuthController.java
@@ -4,13 +4,10 @@ import com.mglvarella.gatewayapi.auth.api.dto.LoginRequest;
 import com.mglvarella.gatewayapi.auth.api.dto.TokenResponse;
 import com.mglvarella.gatewayapi.auth.service.AuthService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.http.HttpResponse;
 
 @RestController("/auth")
 public class AuthController {
@@ -21,8 +18,8 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @PostMapping
-    public ResponseEntity<TokenResponse> auth(@Valid @RequestBody LoginRequest loginRequest) {
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok(this.authService.login(loginRequest));
     }
 }
